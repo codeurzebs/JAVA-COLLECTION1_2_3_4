91d5d12b520ddf4fdf11720f91e7fc170c091bcb
NGUENAZEBS <120027323+NGUENAZEBS@users.noreply.github.com>
2022-12-24 09:19:29 +0100

    Update TestNombre.java

--- src/main/java/com/nguenazebs/exercise3/TestNombre.java
+++ src/main/java/com/nguenazebs/exercise3/TestNombre.java
@@ -45,3 +45,15 @@ public class TestNombre {
 	}
 
 }
+/* @DEV JAVA ZEBS       
+ * Page site
+ * 24/12/2022
+ * @author          NGUENA ZEBS HAUPUR FAUST
+ * @uri 	    pilotezebs.com
+ * @copyright       Copyright (c) 2022 NGUENA ZEBS HAUPUR FAUST
+ * @license         My Login is licensed under the ECLIPSE license.
+ * @github          https://github.com/NGUENAZEBS
+ * @version         1.0.1 USJ
+ * Help me to keep this project
+ *https://ko-fi.com/pilotezebs
+ */
