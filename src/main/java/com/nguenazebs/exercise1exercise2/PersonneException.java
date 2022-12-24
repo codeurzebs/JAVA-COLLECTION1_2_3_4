1cec178a3de5bf9cb9a2e808d143b69179ed3236
NGUENAZEBS <120027323+NGUENAZEBS@users.noreply.github.com>
2022-12-24 09:20:25 +0100

    Update PersonneException.java

--- src/main/java/com/nguenazebs/exercise1exercise2/PersonneException.java
+++ src/main/java/com/nguenazebs/exercise1exercise2/PersonneException.java
@@ -5,3 +5,15 @@ public class PersonneException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 }
+/* @DEV JAVA ZEBS       
+ * Page site
+ * 24/12/2022
+ * @author          NGUENA ZEBS HAUPUR FAUST
+ * @uri 	    pilotezebs.com
+ * @copyright       Copyright (c) 2022 NGUENA ZEBS HAUPUR FAUST
+ * @license         My Login is licensed under the ECLIPSE license.
+ * @github          https://github.com/NGUENAZEBS
+ * @version         1.0.1 USJ
+ * Help me to keep this project
+ *https://ko-fi.com/pilotezebs
+ */
